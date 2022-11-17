982d3f11f1202db8d1da0922fba064dd2bd93630
Theo <theodorosladasmail@gmail.com>
2022-11-17 18:13:58 +0200

    Added - hero content and image

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        user-select: none;
     }
     
     html {
@@ -34,32 +35,70 @@ const GlobalStyle = createGlobalStyle`
     a {
         text-decoration: none;
     }
+    .container {
+      padding: 5rem 5rem;
+      min-height: 90vh;
+      @media only screen and (max-width: 500px) {
+        padding: 5rem 2rem;
+      }
+    }
+    .hero-content {
+      width: 50%;
+      @media screen and (max-width: 1024px) {
+        width: 100%;
+        margin: auto;
+      }
+    }
+    .hero-image {
+      width: 50%;
+      @media screen and (max-width: 1024px) {
+        width: 100%;
+      }
+    }
+    .main-title {
+      font-family: "Arima", cursive;
+      font-size: 3rem;
+      font-weight: 700;
+      color: #282828;
+      /* padding: 1rem 0; */
+      @media screen and (max-width: 1024px) {
+        text-align: center;
+      }
+      @media only screen and (max-width: 500px) {
+        font-size: 2.5rem;
+      }
+    }
+
+    .katia-logo-text {
+      display: block;
+      padding: 1rem 0 2rem;
+      @media screen and (max-width: 1024px) {
+        margin: auto;
+      }
+    }
 `;
-export const Container = styled.div`
-  max-width: 1200px;
-  padding: 5rem 10rem;
-  min-height: 90vh;
-  @media only screen and (max-width: 500px) {
-    padding: 5rem 2rem;
-  }
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
-export const Title = styled.h1`
-  font-family: "Arima", cursive;
-  font-size: 3rem;
-  font-weight: 700;
-  color: #282828;
-  @media only screen and (max-width: 500px) {
-    font-size: 2.5rem;
-  }
-`;
+
 export const Paragraph = styled.p`
   font-family: "Comfortaa", cursive;
   font-size: 1.135rem;
-  text-align: justify;
+  text-align: left;
   line-height: 30px;
   color: #282828;
   max-width: 80%;
   margin-top: 0.5rem;
+  padding-top: 0.5rem;
+
+  @media screen and (max-width: 1024px) {
+    text-align: center;
+    max-width: 100%;
+  }
   @media only screen and (max-width: 500px) {
     max-width: 90%;
   }
@@ -76,6 +115,18 @@ export const CTAButton = styled.a`
     background: #e4389b;
   }
 `;
+export const HeroImg = styled.img`
+  max-width: 90%;
+  height: auto;
+  margin-left: auto;
+  object-fit: cover;
+  display: block;
+  @media screen and (max-width: 1024px) {
+    margin: auto;
+    width: 80%;
+  }
+`;
+
 export const CTACall = styled.a`
   color: #f49ac1;
   letter-spacing: 1px;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,50 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import GlobalStyle from "../components/GlobalStyle.js";
+import homepageHeader from "../img/homepage-header.png";
 import {
-  Container,
-  Title,
   Paragraph,
   CTACall,
+  HeroImg,
+  Flex,
 } from "../components/GlobalStyle.js";
 import { PrimaryBtn } from "../components/PrimaryBtn.js";
 
 const Homepage = () => {
   return (
-    <StyledHomepage>
-      <Container>
-        <Title className="title">Σύγχρονο Κέντρο Αισθητικής</Title>
-        <img
-          src="https://fontmeme.com/permalink/221116/51064fdbe2bec7d4de2bfdf93d4c3e88.png"
-          alt="daniel-font"
-          border="0"
-          className="katia"
-        ></img>
-        {/* <img
+    <div className="homepage">
+      <Flex className="container">
+        <div className="hero-content">
+          <div className="main-title">Σύγχρονο Κέντρο Αισθητικής</div>
+          <img
+            src="https://fontmeme.com/permalink/221116/51064fdbe2bec7d4de2bfdf93d4c3e88.png"
+            alt="daniel-font"
+            border="0"
+            className="katia-logo-text"
+          ></img>
+          {/* <img
           className="katia"
           src="https://fontmeme.com/permalink/221116/c50eb2d83ce446642df0ace149174f35.png"
           alt="Katia Lada Logo"
           border="0"
         ></img> */}
-        <Paragraph>
-          Επαγγελματικές Υπηρεσίες περιποίηση άκρων, μακιγιάζ, τοποθέτηση
-          βλεφαρίδων lashlift, αποτρίχωσης και περιποίησης προσώπου.
-        </Paragraph>
-        <Paragraph>
-          Για ραντεβού μπορείτε να καλέσετε στο{" "}
-          <CTACall href="tel:+00302374082034">2374082034</CTACall>, στις ώρες
-          λειτουργίας μας.
-        </Paragraph>
-        <Paragraph>Δευ - Παρ: 10:00 - 13:00 | 15:00 - 20:00</Paragraph>
-        {/* <div class="pattern-dots-md gray-light"> */}
-        <PrimaryBtn />
-      </Container>
-    </StyledHomepage>
+          <Paragraph>
+            Επαγγελματικές Υπηρεσίες περιποίηση άκρων, μακιγιάζ, τοποθέτηση
+            βλεφαρίδων lashlift, αποτρίχωσης και περιποίησης προσώπου.
+          </Paragraph>
+          <Paragraph>
+            Για ραντεβού μπορείτε να καλέσετε στο{" "}
+            <CTACall href="tel:+00302374082034">2374082034</CTACall>, στις ώρες
+            λειτουργίας μας.
+          </Paragraph>
+          <Paragraph>Δευ - Παρ: 10:00 - 13:00 | 15:00 - 20:00</Paragraph>
+          {/* <div class="pattern-dots-md gray-light"> */}
+          <PrimaryBtn />
+        </div>
+        <div className="hero-image">
+          <HeroImg src={homepageHeader}></HeroImg>
+        </div>
+      </Flex>
+    </div>
   );
 };
 
-const StyledHomepage = styled.div`
-  .katia {
-    padding: 1rem 0;
-  }
-`;
 export default Homepage;
