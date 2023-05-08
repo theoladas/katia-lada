e16f81b0c302fe9af4c2d58d73cf1f59a58a68b5
Theo <theodorosladasmail@gmail.com>
2023-05-08 23:05:58 +0100

    Add onScroll animation for the Homepage component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import { React, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
+import { InView } from "react-intersection-observer";
 import { Paragraph, CTACall, Flex } from "../components/GlobalStyle.js";
 import { PrimaryBtn } from "../components/PrimaryBtn.js";
 import { ServicesSection } from "../components/ServicesSection.js";
@@ -13,6 +14,11 @@ import katiaLogo from "../img/katia-lada-text.png";
 const Homepage = () => {
   const { t, i18n } = useTranslation();
 
+  const [servicesInView, setServicesInView] = useState(false);
+  const [galleryInView, setGalleryInView] = useState(false);
+  const [ourStoreInView, setOurStoreInView] = useState(false);
+  const [contactInView, setContactInView] = useState(false);
+
   const dropIn = {
     hidden: { opacity: 0, y: -50 },
     visible: { opacity: 1, y: 0, transition: { duration: 1 } },
@@ -21,6 +27,11 @@ const Homepage = () => {
   const riseIn = {
     hidden: { opacity: 0, y: 50 },
     visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1, ease: "easeInOut" } },
   };
 
   return (
@@ -85,10 +96,58 @@ const Homepage = () => {
           </motion.div>
         </div>
       </Flex>
-      <ServicesSection />
-      <GallerySection />
-      <OurStoreSection />
-      <ContactSection />
+      <InView
+        as="div"
+        threshold={0.1}
+        onChange={(inView) => setServicesInView(inView)}
+      >
+        <motion.div
+          initial={servicesInView ? "visible" : "hidden"}
+          animate={servicesInView ? "visible" : "hidden"}
+          variants={fadeIn}
+        >
+          <ServicesSection />
+        </motion.div>
+      </InView>{" "}
+      <InView
+        as="div"
+        threshold={0.1}
+        onChange={(inView) => setGalleryInView(inView)}
+      >
+        <motion.div
+          initial={galleryInView ? "visible" : "hidden"}
+          animate={galleryInView ? "visible" : "hidden"}
+          variants={fadeIn}
+        >
+          <GallerySection />
+        </motion.div>
+      </InView>{" "}
+      <InView
+        as="div"
+        threshold={0.1}
+        onChange={(inView) => setOurStoreInView(inView)}
+      >
+        <motion.div
+          initial={ourStoreInView ? "visible" : "hidden"}
+          animate={ourStoreInView ? "visible" : "hidden"}
+          variants={fadeIn}
+        >
+          <OurStoreSection />
+        </motion.div>
+      </InView>
+      <InView
+        as="div"
+        threshold={0.1}
+        onChange={(inView) => setContactInView(inView)}
+      >
+        <motion.div
+          initial={contactInView ? "visible" : "hidden"}
+          animate={contactInView ? "visible" : "hidden"}
+          variants={fadeIn}
+        >
+          <ContactSection />
+        </motion.div>
+      </InView>
     </main>
   );
 };
