c8d248e64d0a944a791595116fd67c23bbcb7561
Theo <theodorosladasmail@gmail.com>
2023-05-12 17:28:41 +0100

    Make GallerySection grid dynamic

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -3,12 +3,7 @@ import { Flex, Paragraph } from "../components/GlobalStyle";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
-import grid1 from "../img/grid-1.png";
-import grid2 from "../img/grid-2.png";
-import grid3 from "../img/grid-3.png";
-import grid4 from "../img/grid-4.png";
-import grid5 from "../img/grid-5.png";
-import grid6 from "../img/grid-6.png";
+import gallerySectionData from "../resources/gallerySection-data.json";
 import "../styles/GallerySection.scss";
 
 export const GallerySection = ({ animate }) => {
@@ -47,75 +42,20 @@ export const GallerySection = ({ animate }) => {
             animate={{ x: 0, ...fadeIn.visible }}
             transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
           >
-            <div className="grid-column">
-              <motion.img
-                src={grid1}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.3 }}
-              />
-              <motion.img
-                src={grid6}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.4 }}
-              />
-              <motion.img
-                src={grid5}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.5 }}
-              />
-            </div>
-            <div className="grid-column">
-              <motion.img
-                src={grid2}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.3 }}
-              />
-              <motion.img
-                src={grid4}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.4 }}
-              />
-              <motion.img
-                src={grid6}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.5 }}
-              />
-            </div>
-            <div className="grid-column">
-              <motion.img
-                src={grid3}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.3 }}
-              />
-              <motion.img
-                src={grid5}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.4 }}
-              />
-              <motion.img
-                src={grid4}
-                alt="Gallery example"
-                initial={{ ...fadeIn.hidden }}
-                animate={{ ...fadeIn.visible }}
-                transition={{ delay: 0.5 }}
-              />
-            </div>
+            {gallerySectionData.map((column, columnIndex) => (
+              <div className="grid-column" key={columnIndex}>
+                {column.map((image, index) => (
+                  <motion.img
+                    src={image.src}
+                    alt={image.alt}
+                    initial={{ ...fadeIn.hidden }}
+                    animate={{ ...fadeIn.visible }}
+                    transition={{ delay: image.delay }}
+                    key={index}
+                  />
+                ))}
+              </div>
+            ))}
           </motion.div>
         </>
       )}
