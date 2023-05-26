a53f53aeff2946fa533d20b7e763ebe39d26af1a
Theo <theodorosladasmail@gmail.com>
2023-05-26 14:32:04 +0100

    Create Facial page and add template code

---
+++ src/pages/Facial.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useTranslation } from "react-i18next";
+import Tabs from "../components/Tabs.js";
+import {
+  Page,
+  PageTitle,
+  HeroContent,
+  FlexRowHeroContainer,
+} from "../components/GlobalStyle.js";
+import makeup from "../img/makeup.jpg";
+
+const Facial = () => {
+  const { t } = useTranslation();
+  const dropIn = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  const riseIn = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+    threshold: 0.1, // Percentage of the element that is in view before the callback triggers
+  });
+
+  const tabData = [
+    {
+      id: 1,
+      title: "Standard",
+      description: t("makigiazPerigrafi"),
+      image: "/img/makeup.jpg",
+    },
+    {
+      id: 2,
+      title: t("nifikoMakigiaz"),
+      description: t("nigikoMakigiazPerigrafi"),
+      image: "/img/makeup-bride.jpg",
+    },
+  ];
+  const [selectedTab, setSelectedTab] = useState(tabData[0].id);
+
+  const selectedTabData = tabData.find((tab) => tab.id === selectedTab);
+  const backgroundImage = selectedTabData ? selectedTabData.image : makeup;
+  const handleTabChange = (tabId) => {
+    setSelectedTab(tabId);
+  };
+  return (
+    <Page
+      className="homepage"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <motion.div
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+        variants={dropIn}
+        ref={ref}
+      >
+        <PageTitle className="main-title gallery-title">
+          {t("makigiaz")}
+        </PageTitle>
+      </motion.div>
+
+      <FlexRowHeroContainer>
+        <HeroContent>
+          <motion.div
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+            variants={riseIn}
+            ref={ref}
+          >
+            <Tabs tabs={tabData} onTabChange={handleTabChange} />
+          </motion.div>
+        </HeroContent>
+      </FlexRowHeroContainer>
+    </Page>
+  );
+};
+
+export default Facial;
